428740391185a03bd97170a11be5e3a5d473bfeb
Константин Ковальчук <kostia-kovallyk@yandex.ru>
2025-04-10 18:25:21 +0300

    Create main settings

---
+++ booking_app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunModule(BaseModel):
+    '''
+    Configs for running the project.
+    '''
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api/v1"
+
+
+class Settings(BaseSettings):
+    run: RunModule = RunModule()
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
