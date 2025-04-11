ed3dd29486caf459bc363069ffecf2e1a796e3de
Константин Ковальчук <kostia-kovallyk@yandex.ru>
2025-04-11 16:56:22 +0300

    Create database config

--- booking_app/core/config.py
+++ booking_app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -14,9 +14,19 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api/v1"
 
 
+class DatabaseModule(BaseModel):
+    url: PostgresDsn,
+    echo: bool = False,
+    echo_pool: bool = False,
+    pool_size: int = 50,
+    max_overflow: int = 10,
+
+
+
 class Settings(BaseSettings):
     run: RunModule = RunModule()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseModule
 
 
 settings = Settings()
