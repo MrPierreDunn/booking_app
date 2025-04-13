198b3b19ad7b41cdc9814bfade7e4e3ab1f81346
Константин Ковальчук <kostia-kovallyk@yandex.ru>
2025-04-13 17:28:17 +0300

    Create base model

---
+++ booking_app/core/models/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import MetaData
+
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_conventions
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
