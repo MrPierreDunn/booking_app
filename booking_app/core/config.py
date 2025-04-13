44e7b978ddf1ab6a528390173e17ad1d59fa4f00
Константин Ковальчук <kostia-kovallyk@yandex.ru>
2025-04-13 17:33:25 +0300

    Update config

--- booking_app/core/config.py
+++ booking_app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunModule(BaseModel):
@@ -15,18 +15,31 @@ class ApiPrefix(BaseModel):
 
 
 class DatabaseModule(BaseModel):
-    url: PostgresDsn,
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
-    max_overflow: int = 10,
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=("booking_app/.env.template", "booking_app/.env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="FASTAPI_CONFIG__",
+    )
     run: RunModule = RunModule()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseModule
 
 
-settings = Settings()
+settings = Settings(db={"url": "postgresql+asyncpg://user:password@localhost:5432/booking"})
