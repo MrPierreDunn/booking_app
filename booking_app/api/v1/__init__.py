7d00119ddd05b0f4c896ca1d499897d03d6a0677
Константин Ковальчук <kostia-kovallyk@yandex.ru>
2025-04-14 15:36:38 +0300

    Register routers for endpoints

--- booking_app/api/v1/__init__.py
+++ booking_app/api/v1/__init__.py
@@ -1,3 +1,19 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+
+from .tables import router as tables_router
+from .reservations import router as reservations_router
+
+router = APIRouter(
+    prefix=settings.api.v1.prefix,
+)
+
+router.include_router(
+    tables_router,
+    prefix=settings.api.v1.tables,
+)
+router.include_router(
+    reservations_router,
+    prefix=settings.api.v1.reservations,
+)
